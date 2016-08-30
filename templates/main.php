489bf00dc06e7430808ad0596c7bd2b1f751d866
Unknown <janiskoehr@icloud.com>
2016-08-30 13:09:01 +0200

    Merged stable10 into master

--- templates/main.php
+++ templates/main.php
@@ -9,26 +9,13 @@
  * @copyright Janis Koehr 2016
  */
 //Select2.js for the tag field
-//TODO: call of static method not allowed in app:check
-\OC_Util::addVendorScript('select2/select2');
-\OC_Util::addVendorStyle('select2/select2');
-//simplemde.js for the WYSIWYG .md Editor
-\OCP\Util::addScript('nextnotes', 'simplemde.min');
-//clearsearch.js - self made: clearIcon 'X' in the searchfield
-\OCP\Util::addScript('nextnotes', 'clearsearch.min');
-//main app with all necessary classes and ajax call functions
-\OCP\Util::addScript('nextnotes', 'nextnotesapp');
-\OCP\Util::addScript('nextnotes', 'nextnotesnotes');
-\OCP\Util::addScript('nextnotes', 'nextnotestags');
-\OCP\Util::addScript('nextnotes', 'nextnotesview');
-//Font-Awesome for pretty icons in SimpleMDE
-\OCP\Util::addStyle('nextnotes', 'font-awesome.min');
-//SimpleMDE styling
-\OCP\Util::addStyle('nextnotes', 'simplemde.min');
-//Markdown.css for beautiful preview of Notes (usability and apparel)
-\OCP\Util::addStyle('nextnotes', 'markdown.min');
-//main styling and css fixes for the app.
-\OCP\Util::addStyle('nextnotes', 'style');
+vendor_script('select2/select2');
+vendor_style('select2/select2');
+//App scripts and styles
+$scripts = array('simplemde.min', 'clearsearch.min', 'nextnotesapp', 'nextnotesnotes', 'nextnotestags', 'nextnotesview');
+script('nextnotes', $scripts);
+$styles = array('font-awesome.min', 'simplemde.min', 'markdown.min', 'style');
+style('nextnotes', $styles);
 ?>
 <div id="app">
 	<!-- nextCloud - nextnotes - This file is licensed under the Affero General Public License version 3 or later. See the COPYING file. - @author Janis Koehr <janiskoehr@icloud.com> - @copyright Janis Koehr 2016 -->
